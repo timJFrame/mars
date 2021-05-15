7b274960688ed3ebd980c0e9838b9e7ac6067c7a
timframe <t.f.retouching@gmail.com>
2021-05-15 17:46:33 +0100

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Home from './components/Home'
-import Nav from './components/Nav'
+import Navigation from './components/Navigation'
 
 function App(){
   return(
     <BrowserRouter>
-    <Nav/>
+    <Navigation/>
     <Switch>
       <Route exact path ="/" component={Home}/>
     </Switch>
